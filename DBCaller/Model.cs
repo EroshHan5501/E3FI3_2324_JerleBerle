d9a4a1e2330cbceab94416b0a4ce1b3355269404
Eren UEzer <eren.uezer@theobald-software.com>
2023-11-05 22:48:23 +0100

    Add Entity Framework

---
+++ DBCaller/Model.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBCaller
+{
+    public class RecipeContext : DbContext
+    {
+
+        public DbSet<recipe> Recipees { get; set; }
+        public DbSet<ingredient> Ingredients { get; set; }
+
+        public string DbPath { get; }
+
+        public RecipeContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "recipe.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+
+    public class recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UPM { get; set; }
+    }
+}
---
+++ DBCaller/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DBCaller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
