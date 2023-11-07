e95272dedb2b76927b7c2c29c1e2356cfbde7645
Ron Bruetsch <ron.bruetsch@itschulestuttgart1.onmicrosoft.com>
2023-11-07 08:57:04 +0100

    Starting sql script for dummy data

---
+++ DummyData.sql
@@ -0,0 +1,6 @@
+USE recipeAppDb;
+
+INSERT INTO User(username, email, password, role) VALUES('admin', 'admin.test@gmail.com', '+5l9XAHrz5Ytggs7Dn+L/ut/S9M3zINoLyr5DSUsIMXYV0S3xruU9IE59pCmHkrTF9YQfkMQ78AW2ShyZrUXKw==', 1);
+INSERT INTO User(username, email, password, role) VALUES('test1', 'test1.test@gmail.com', '+5l9XAHrz5Ytggs7Dn+L/ut/S9M3zINoLyr5DSUsIMXYV0S3xruU9IE59pCmHkrTF9YQfkMQ78AW2ShyZrUXKw==', 0);
+INSERT INTO User(username, email, password, role) VALUES('test2', 'test2.test@gmail.com', '+5l9XAHrz5Ytggs7Dn+L/ut/S9M3zINoLyr5DSUsIMXYV0S3xruU9IE59pCmHkrTF9YQfkMQ78AW2ShyZrUXKw==', 0);
+INSERT INTO User(username, email, password, role) VALUES('test3', 'test3.test@gmail.com', '+5l9XAHrz5Ytggs7Dn+L/ut/S9M3zINoLyr5DSUsIMXYV0S3xruU9IE59pCmHkrTF9YQfkMQ78AW2ShyZrUXKw==', 0);
