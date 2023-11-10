1ca01cd10e5c6e6d5ab4630039c3638a55b09156
Ron Bruetsch <ron.bruetsch@itschulestuttgart1.onmicrosoft.com>
2023-11-10 09:16:14 +0100

    Writing a basic builder for file system creation

---
+++ RecipeCommon/Testing/FileContentBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+
+namespace RecipeCommon.Testing;
+
+public class FileContentBuilder
+{
+    public string Filepath { get; set; }
+
+    public List<string> Content { get; private set; }
+
+    public FileContentBuilder(string filepath)
+    {
+        Filepath = filepath;
+        Content = new List<string>();
+    }
+    
+    public void WithText(string text)
+    {
+        Content.Add(text);
+    }
+
+    public void WithJson<TEntity>(TEntity entity) 
+        where TEntity : class
+    {
+        string json = JsonSerializer.Serialize<TEntity>(entity);
+        Content.Add(json);  
+    }
+
+    public void Build()
+    {
+        File.WriteAllLines(Filepath, Content);
+    }
+}
