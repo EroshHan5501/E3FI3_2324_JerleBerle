922adb73c6398e3027cfcacb62c10081b865bca9
Ron Bruetsch <ron.bruetsch@itschulestuttgart1.onmicrosoft.com>
2023-11-10 16:38:55 +0100

    Finish tests for the builder

--- RecipeCommon.Tests/FileSystem/DirectoryBuilderShould.cs
+++ RecipeCommon.Tests/FileSystem/DirectoryBuilderShould.cs
@@ -1,101 +1,138 @@
 ﻿using RecipeCommon.Testing;
 using Xunit;
 
-namespace RecipeCommon.Tests.FileSystem
+namespace RecipeCommon.Tests.FileSystem;
+
+public class DirectoryBuilderShould
 {
-    public class DirectoryBuilderShould
+    [Fact]
+    public void CreateEmptyDirectoryWithUuidAsName()
     {
-        [Fact]
-        public void CreateEmptyDirectoryWithUuidAsName()
-        {
-            DirectoryEnvironment env = new DirectoryBuilder().Build();
+        DirectoryEnvironment env = new DirectoryBuilder().Build();
 
-            Assert.True(Directory.Exists(env.BasePath));
-        }
+        Assert.True(Directory.Exists(env.BasePath));
+    }
 
-        [Fact]
-        public void CreateFilesInsideBaseDirectory()
-        {
-            DirectoryBuilder builder = new DirectoryBuilder();
+    [Fact]
+    public void CreateFilesInsideBaseDirectory()
+    {
+        DirectoryBuilder builder = new DirectoryBuilder();
 
-            DirectoryEnvironment env = builder
-                .WithFile("general.json", null)
-                .WithFile("testing.txt", null)
-                .Build();
+        DirectoryEnvironment env = builder
+            .WithFile("general.json", null)
+            .WithFile("testing.txt", null)
+            .Build();
 
-            Assert.True(Directory.Exists(env.BasePath));
+        Assert.True(Directory.Exists(env.BasePath));
 
-            IEnumerable<string> filepaths = env.GetFilePaths();
+        IEnumerable<string> filepaths = env.GetFilePaths();
 
-            Assert.All(filepaths, path => Assert.True(File.Exists(path)));  
-        }
+        Assert.All(filepaths, path => Assert.True(File.Exists(path)));  
+    }
 
-        internal class JsonEntity
-        {
-            public string Name { get; set; }
+    internal class JsonEntity
+    {
+        public string Name { get; set; }
 
-            public int Value { get; set; }
-        }
+        public int Value { get; set; }
+    }
 
-        [Fact]
-        public void CreateFilesWithCorrectContent()
-        {
-            DirectoryBuilder builder = new DirectoryBuilder();
+    [Fact]
+    public void CreateFilesWithCorrectContent()
+    {
+        DirectoryBuilder builder = new DirectoryBuilder();
 
-            DirectoryEnvironment env = builder
-                .WithFile("general.json", general =>
+        DirectoryEnvironment env = builder
+            .WithFile("general.json", general =>
+            {
+                JsonEntity entity = new()
                 {
-                    JsonEntity entity = new()
-                    {
-                        Name = "Testing",
-                        Value = 300
-                    };
+                    Name = "Testing",
+                    Value = 300
+                };
 
-                    general.WithJson<JsonEntity>(entity);
-                })
-                .WithFile("testing.txt", testing =>
-                {
-                    testing.WithText("Hello world");
-                })
-                .Build();
+                general.WithJson<JsonEntity>(entity);
+            })
+            .WithFile("testing.txt", testing =>
+            {
+                testing.WithText("Hello world");
+            })
+            .Build();
 
-            IEnumerable<string> filepaths = env.GetFilePaths();
+        IEnumerable<string> filepaths = env.GetFilePaths();
 
-            Assert.True(Directory.Exists(env.BasePath));
-            Assert.All(filepaths, path => Assert.True(File.Exists(path)));
+        Assert.True(Directory.Exists(env.BasePath));
+        Assert.All(filepaths, path => Assert.True(File.Exists(path)));
 
-            string expectedJson ="{\"Name\":\"Testing\",\"Value\":300}\r\n";
+        string expectedJson ="{\"Name\":\"Testing\",\"Value\":300}\r\n";
 
-            string actualJson = File.ReadAllText(Path.Combine(env.BasePath, "general.json"));
-            Assert.Equal(expectedJson, actualJson);
+        string actualJson = File.ReadAllText(Path.Combine(env.BasePath, "general.json"));
+        Assert.Equal(expectedJson, actualJson);
 
-            string expectedText = "Hello world\r\n";
+        string expectedText = "Hello world\r\n";
 
-            string actualText = File.ReadAllText(Path.Combine(env.BasePath, "testing.txt"));
+        string actualText = File.ReadAllText(Path.Combine(env.BasePath, "testing.txt"));
 
-            Assert.Equal(expectedText, actualText);
-        }
+        Assert.Equal(expectedText, actualText);
+    }
 
-        [Fact]
-        public void CreatesMultipleFilesInBaseDirectory()
+    [Fact]
+    public void CreatesMultipleFilesInBaseDirectory()
+    {
+        DirectoryBuilder builder = new DirectoryBuilder();
+
+        DirectoryEnvironment env = builder.WithFiles(
+            new List<(string, Action<FileContentBuilder>?)>()
+            {
+                ("general.json", null),
+                ("testing.txt", null),
+                ("markdown.md", null)
+            })
+            .Build();
+
+        IEnumerable<string> filepaths = env.GetFilePaths();
+
+        int count = Directory.GetFileSystemEntries(env.BasePath, "*", SearchOption.AllDirectories).Count();
+        Assert.Equal(count, filepaths.Count());
+
+        Assert.All(filepaths, path => Assert.True(File.Exists(path)));
+    }
+
+    public static IEnumerable<object[]> GetEqualityTestingData()
+    {
+        yield return new object[]
         {
-            DirectoryBuilder builder = new DirectoryBuilder();
+            new FileContentMapping("folder1", builder => builder.WithText("Hello world")),
+            true
+        };
 
-            DirectoryEnvironment env = builder.WithFiles(
-                new List<(string, Action<FileContentBuilder>?)>()
-                {
-                    ("general.json", null),
-                    ("testing.txt", null),
-                    ("markdown.md", null)
-                })
-                .Build();
+        yield return new object[]
+        {
+            new FileContentMapping("folder2", builder => builder.WithText("Hello world")),
+            false
+        };
 
-            IEnumerable<string> filepaths = env.GetFilePaths();
+        yield return new object[]
+        {
+            new FileContentMapping("123456", builder => builder.WithText("Hello world")),
+            false
+        };
 
-            int count = Directory.GetFileSystemEntries(env.BasePath, "*", SearchOption.AllDirectories).Count();
-            Assert.Equal(count, filepaths.Count());
+        yield return new object[]
+        {
+            new FileContentMapping("hello", builder => builder.WithText("Hello world")),
+            false
+        };
+    }
 
-            Assert.All(filepaths, path => Assert.True(File.Exists(path)));
-        }
+    [Theory]
+    [MemberData(nameof(GetEqualityTestingData))]
+    public void ReturnsCorrectEqualityForMapping(FileContentMapping mapping, bool expected)
+    {
+        FileContentMapping testing = new FileContentMapping("folder1", null);
+
+        bool actual = testing.Equals(mapping);
+
+        Assert.Equal(expected, actual);
     }
 }
