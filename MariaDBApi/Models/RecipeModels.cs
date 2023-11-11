63408960de6de3537e381aaacc82ec3b582f620f
EroshHan5501 <eren.uezer@outlook.de>
2023-11-11 14:30:32 +0100

    Folder stuff

---
+++ MariaDBApi/Models/RecipeModels.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MariaDBApi.Models
+{
+    public class RecipeContext : DbContext
+    {
+        public RecipeContext(DbContextOptions<RecipeContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Recipe> RecipeItems { get; set; } = null!;
+        public DbSet<Ingredient> IngredientItems { get; set; } = null!;
+    }
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class UnitOfMeasurement
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class RI_Rel
+    {
+
+    }
+}
