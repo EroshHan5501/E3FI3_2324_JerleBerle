c9c9c91323d681b2e791cbb3317a48f9154cdb2d
EroshHan5501 <eren.uezer@outlook.de>
2023-11-17 23:00:20 +0100

    Add DbContext

---
+++ RecipeAPI/Database/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RecipeAPI.Database
+{
+    public class AppDbContext : DbContext
+    {
+        string connectionString = "Server=localhost; User=root; Password=Gatling762; Database=recipeapp";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+    }
+}
