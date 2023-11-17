a109cf67b7b4ed4c15a6e840963e04b9c3cfda9a
Ron Bruetsch <ron.bruetsch@itschulestuttgart1.onmicrosoft.com>
2023-11-17 09:45:00 +0100

    Implement ingredient controller for data provider API

---
+++ RecipeApp/DataProviderApi/Models/RecipeModels.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataProviderApi.Models;
+
+public class RecipeContext : DbContext
+{
+    public RecipeContext(DbContextOptions<RecipeContext> options)
+    : base(options)
+    {
+    }
+
+    public DbSet<Recipe> RecipeItems { get; set; } = null!;
+    public DbSet<Ingredient> IngredientItems { get; set; } = null!;
+}
+
+[Table("Recipe")]
+public class Recipe
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+[Table("Ingredient")]
+public class Ingredient
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class UnitOfMeasurement
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class RI_Rel
+{
+
+}
