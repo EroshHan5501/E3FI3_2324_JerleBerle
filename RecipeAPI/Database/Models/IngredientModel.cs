9996b2c7424a0438341361fad94f9c1f7332852c
Ron Bruetsch <ron.bruetsch@itschulestuttgart1.onmicrosoft.com>
2023-11-17 10:42:47 +0100

    Add ingredient model

---
+++ RecipeAPI/Database/Models/IngredientModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RecipeAPI.Database.Models;
+
+public class IngredientModel
+{
+    [Key]
+    [Column("Id")]
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+}
