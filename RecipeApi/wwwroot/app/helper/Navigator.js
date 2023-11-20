55962664a3a5d67db986a0cc8e6079df1a962e74
Ron Bruetsch <ron.bruetsch@itschulestuttgart1.onmicrosoft.com>
2023-11-20 12:45:19 +0100

    Implement basic navigation

--- RecipeApi/wwwroot/app/helper/Navigator.js
+++ RecipeApi/wwwroot/app/helper/Navigator.js
@@ -3,25 +3,24 @@ import { RouteNames } from "./RouteNames.js";
 export class Navigator {
 
     static async goToAsync(path) {
-
         let page = null;
 
-        switch(path) {
+        switch (path) {
             case RouteNames.login: {
                 const module = await import("/app/auth/login/login.js");
                 page = new module.LoginPage();
             }
-            break;
+                break;
             case RouteNames.register: {
                 const module = await import("/app/auth/register/register.js");
                 page = new module.RegisterPage();
             }
-            break;
+                break;
             case RouteNames.logout: {
                 const module = await import("/app/auth/logout/logout.js");
                 page = new module.LogoutPage();
             }
-            break;
+                break;
             case RouteNames.dashboard: {
                 const module = await import("/app/dashboard/dashboard.js");
                 page = new module.DashboardPage();
@@ -30,6 +29,8 @@ export class Navigator {
                 // Error page
                 break;
         }
+
+        window.history.pushState(null, null, path);
 
         const main = document.querySelector("main");
         main.innerText = "";
--- RecipeApi/wwwroot/index.js
+++ RecipeApi/wwwroot/index.js
@@ -1,6 +1,11 @@
 import { Navigator } from "./app/helper/Navigator.js";
 import { RouteNames } from "./app/helper/RouteNames.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-    Navigator.goToAsync(RouteNames.dashboard);
+document.addEventListener("DOMContentLoaded", async () => {
+    const path = document.location.pathname;
+    if (path === "/")
+        await Navigator.goToAsync(RouteNames.dashboard);
+    else {
+        await Navigator.goToAsync(path.substring(1));
+    }
 })
