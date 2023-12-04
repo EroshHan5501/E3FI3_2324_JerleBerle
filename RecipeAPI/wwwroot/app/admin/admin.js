2971c6c4796afcff845ed111a733f8a143419321
Ron Brütsch <ron.bruetsch@theobald-software.com>
2023-12-04 15:12:16 +0100

    Implement client-side pagination

--- RecipeAPI/wwwroot/app/admin/admin.js
+++ RecipeAPI/wwwroot/app/admin/admin.js
@@ -2,6 +2,8 @@ import { AppConfig } from "../helper/AppConfig.js";
 import { MainPage } from "../mainpage/mainpage.js";
 
 export class AdminPage extends MainPage {
+    static #entityNumber = 2;
+    #currentIndex = 1;
     constructor() {
         super({ "title": "Admin panel", "heading": "Admin panel" });
         this.setInitCallback = this.initAsync;
@@ -15,18 +17,55 @@ export class AdminPage extends MainPage {
             this.getPageContentElement);
 
         this.guiContent = {
-            tableBody: this.querySelector("tbody")
+            tableBody: this.querySelector("tbody"),
+            loadBtn: this.querySelector(".load-button")
         }
 
-        const url = AppConfig.buildApiPath("admin/User/?pageIndex=1&pageSize=1");
+        const url = AppConfig.buildApiPath(
+            `admin/User/?pageIndex=1&pageSize=${AdminPage.#entityNumber}`);
 
-        this.getDataAsync(
+        await this.getDataAsync(
             url,
             {
                 400: (statusCode, data) => console.log(data),
                 404: (statusCode, data) => console.log("Could not found!")
             },
             this.#initTableAsync.bind(this));
+
+        // register event handler for the 
+        const delBtns = this.querySelectorAll(".del-btn");
+        delBtns.forEach(btn => this.handleClick(
+            btn,
+            this.#handleDeleteAsync.bind(this)));
+
+        this.handleClick(
+            this.guiContent.loadBtn,
+            this.#handleLoadMoreAsync.bind(this));
+    }
+
+    async #handleLoadMoreAsync(event) {
+        event.preventDefault();
+        this.#currentIndex += 1;
+        const url = AppConfig.buildApiPath(
+            `admin/User/?pageIndex=${this.#currentIndex}&pageSize=${AdminPage.#entityNumber}`);
+
+        await this.getDataAsync(
+            url,
+            {
+                400: (statusCode, data) => console.log(data)
+            },
+            this.#initTableAsync.bind(this));
+    }
+
+    async #handleDeleteAsync(event) {
+        event.preventDefault();
+        const url = AppConfig.buildApiPath(
+            `admin/User/delete?email=${event.target.getAttribute("data")}`);
+
+        await this.deleteDataAsync(
+            url,
+            { 400: (status, data) => console.log(data) },
+            this.initAsync.bind(this));
     }
 
     async #initTableAsync(json) {
@@ -39,16 +78,14 @@ export class AdminPage extends MainPage {
             const id = tmp.querySelector(".id-cell"),
                 username = tmp.querySelector(".username-cell"),
                 email = tmp.querySelector(".email-cell"),
-                role = tmp.querySelector(".role-cell");
-
-            console.log(user);
+                role = tmp.querySelector(".role-cell"),
+                delBtn = tmp.querySelector(".del-btn");
 
             id.innerText = user.id;
             username.innerText = user.username;
             email.innerText = user.email;
             role.innerText = user.role;
-
-            console.log(tmp);
+            delBtn.setAttribute("data", user.email);
 
             this.guiContent.tableBody.appendChild(tmp);
         }
