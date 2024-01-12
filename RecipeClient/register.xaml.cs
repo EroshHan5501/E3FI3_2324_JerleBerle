ae4230bc2b60adef94630ab472726fee4dc01e4a
JanWidmaier <jw@becklyn.com>
2024-01-12 11:14:51 +0100

    rename class

--- RecipeClient/register.xaml.cs
+++ RecipeClient/register.xaml.cs
@@ -18,9 +18,9 @@ namespace Recipe_Client
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Register : Window
     {
-        public MainWindow()
+        public Register()
         {
             InitializeComponent();
         }
